9001d10aa1033076a55cfa90dee6624f8dcd01d9
Laura-Hassert <laura.hassert@gmail.com>
2021-09-23 21:06:05 -0400

    FINAL

--- group-by.sql
+++ group-by.sql
@@ -1,1 +1,18 @@
 1. 
+SELECT g.name, COUNT(t.name) FROM genre g
+JOIN track t ON g.genre_id = t.genre_id
+GROUP BY g.name
+ORDER BY g.name;
+
+2. 
+SELECT g.name, COUNT (*) FROM track t
+JOIN genre g ON g.genre_id = t.genre_id
+WHERE g.name = 'Pop' OR g.name = 'Rock'
+GROUP BY g.name
+ORDER BY g.name;
+
+3. 
+SELECT a.name, COUNT(al.title) FROM artist a
+JOIN album al ON al.artist_id = a.artist_id
+GROUP BY a.name
+ORDER BY a.name;
